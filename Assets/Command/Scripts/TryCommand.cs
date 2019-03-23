b98a3147e198ec62e2220da2a105d51a8412b526
InoueSota <asitanobento87@gmail.com>
2019-03-23 19:07:32 +0900

    お試し版書いた。

--- Assets/Command/Scripts/TryCommand.cs
+++ Assets/Command/Scripts/TryCommand.cs
@@ -4,13 +4,71 @@ using UnityEngine;
 
 public class TryCommand : MonoBehaviour {
 
-	// Use this for initialization
+    //InvokerがCommandを受け取り、コマンド実行する。
+    //その時内部のcommandで渡されたReceiverが実際にコマンドの中身を実行する
+
 	void Start () {
-		
+        JumpBehaveReceiver jumpBehaverReceiver = new JumpBehaveReceiver();
+        JumpCommand jumpCommand = new JumpCommand(jumpBehaverReceiver);
+        CommandInvoker invoker = new CommandInvoker();
+
+        invoker.SetCommand(jumpCommand);
+        invoker.ExecuteCommand();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
+
+interface ICommandReceiver
+{
+    void CommandAction();
+}
+
+class JumpBehaveReceiver : ICommandReceiver
+{
+    public void CommandAction()
+    {
+        Debug.Log("Jump!");
+    }
+}
+
+abstract class Command
+{
+    protected ICommandReceiver _receiver;
+
+    public Command(ICommandReceiver r)
+    {
+        _receiver = r;
+    }
+
+    public abstract void Execute();
+}
+
+class JumpCommand : Command
+{
+    public JumpCommand(JumpBehaveReceiver r) : base(r)
+    {
+
+    }
+
+    public override void Execute()
+    {
+        _receiver.CommandAction();
+    }
+}
+
+
+class CommandInvoker
+{
+    private Command _command;
+
+    public void SetCommand(Command command)
+    {
+        this._command = command;
+    }
+
+    public void ExecuteCommand()
+    {
+        _command.Execute();
+    }
+}
+
